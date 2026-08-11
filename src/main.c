3a961380150217a4cd01af8ec1bd64b9596952e0
NikBuilds <eof0xffff@tutamail.com>
2026-08-11 19:02:38 +0200

    initial commit

---
+++ src/main.c
@@ -0,0 +1,217 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <linux/limits.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+#define RESET "\x1B[0m"
+
+char* read_line() 
+{
+    char *line = NULL;
+    char *tmp = NULL;
+    size_t size = 0, index = 0;
+    int ch = EOF;
+
+    while (ch) {
+        ch = getc(stdin);                       // Reads one char from standard input stdin 
+        if(ch == EOF || ch == '\n') ch = 0;     // 0 = false so loop stops
+
+        if(size <= index) {
+            size += 1;
+            tmp = realloc(line, size); 
+
+            if(!tmp) {
+                free(line);
+                line = NULL;
+                break;
+            }
+            line = tmp;
+        }
+
+        /* Store Chars into string. */
+        line[index++] = ch;
+    }
+    return line;
+}
+
+typedef struct {
+    char **command_line;
+    int count;
+} Command;
+
+// Hier realloc 2d array
+Command split_command(char *line) 
+{
+    Command cmd_struct;
+    cmd_struct.command_line = NULL;
+    cmd_struct.count = 0;
+    char *saveptr;          // Pos for token
+    
+    char *token = strtok_r(line, " ", &saveptr); // Splits string unsing " " as delimiter
+
+    while (token != NULL) { // Loops trough the tokens
+        char **temp = realloc(cmd_struct.command_line, 
+                             (cmd_struct.count + 1) * sizeof(char*));
+
+        if(temp == NULL) {
+            perror("Realloc failed");
+            break;
+        }
+
+        cmd_struct.command_line = temp;
+        cmd_struct.command_line[cmd_struct.count] = NULL; // execvp() needs pointer to NULL at the end of the cmd
+
+        // Speicher für das Wort selbst reservieren
+        cmd_struct.command_line[cmd_struct.count] = malloc(strlen(token) + 1); // + 1 for Nullbyte '\0'
+        if (cmd_struct.command_line[cmd_struct.count] == NULL) {
+            perror("malloc failed");
+            break;
+        }
+
+        // Copy token into command_line
+        strcpy(cmd_struct.command_line[cmd_struct.count], token);
+        cmd_struct.count++;
+
+        token = strtok_r(NULL, " ", &saveptr);
+    }
+
+    return cmd_struct;
+}
+
+void free_command(Command *cmd) 
+{
+    for (int i = 0; i < cmd->count; i++) {
+        free(cmd->command_line[i]);
+    }
+    free(cmd->command_line);
+}
+
+typedef struct {
+    char **sub_folders;
+    char *root_path;
+    int count;
+} Directory;
+
+int create_dir(Directory *subf) 
+{
+    subf->sub_folders = NULL;
+    subf->root_path = NULL;
+    subf->count = 0; 
+
+    DIR *dir = opendir("."); // Opens current directory
+
+    if (!dir) {
+        perror("opendir");
+        return 1;
+    }
+
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)) == NULL) perror("Faild to get the current directory");
+
+    subf->root_path = malloc(strlen(cwd));
+    strcpy(subf->root_path, cwd);
+
+    struct dirent *entry;
+    struct stat st;
+
+    while ((entry = readdir(dir)) != NULL) {
+
+        // Skip . (Current dir) and .. (root dir)
+        if (strcmp(entry->d_name, ".") == 0 ||
+            strcmp(entry->d_name, "..") == 0) {
+            continue;
+        }
+
+        if (stat(entry->d_name, &st) == 0 && S_ISDIR(st.st_mode)) {
+            char **temp = realloc(subf->sub_folders,  
+                (subf->count + 1) * sizeof(char*));
+
+            if(temp == NULL) {
+                perror("Realloc failed");
+                break;
+            }
+            
+            // Assign memory address
+            subf->sub_folders = temp;
+
+            subf->sub_folders[subf->count] = malloc(strlen(entry->d_name) + 1); // + 1 for Nullbyte '\0'
+            if (subf->sub_folders[subf->count] == NULL) {
+                perror("malloc failed");
+                break;
+            }
+
+            strcpy(subf->sub_folders[subf->count], entry->d_name);
+            subf->count++; 
+        }
+    }
+
+    closedir(dir);
+    return 0;
+}
+
+void free_dir(Directory *dir) 
+{
+    for (int i = 0; i < dir->count; i++) {
+        free(dir->sub_folders[i]);
+    }
+    free(dir->sub_folders);
+}
+
+
+int main() {
+    Directory dir;
+    create_dir(&dir);
+        
+    while(1) {
+        printf("%s[JumpShell] ", KRED);
+        printf("%s%s\n", KCYN, dir.root_path);
+        printf(KCYN "<" KWHT "./" KCYN "> " RESET);
+
+        char *line = read_line();
+        Command cmd = split_command(line);
+
+        if(!strcmp("exit", cmd.command_line[0])) exit(0);
+        if(!strcmp("subf", cmd.command_line[0])) {
+            for(int i = 0; i < dir.count; i++) {
+                printf("/%s\n", dir.sub_folders[i]);
+            }
+            continue;
+        }
+
+        // Create child process
+        int status;
+        pid_t child_pid = fork();
+
+        if(child_pid < 0 ) {
+            perror("fork fail");
+            exit(1);
+        }
+
+        if(child_pid == 0){
+            // Replace child process with the programm
+            execvp(cmd.command_line[0], cmd.command_line);
+            
+            perror("execvp fail");
+            _exit(EXIT_FAILURE);
+        } 
+
+        waitpid(child_pid, &status, 0);
+
+        free_command(&cmd);
+    }
+
+    free_dir(&dir);
+    return 0;
+}
